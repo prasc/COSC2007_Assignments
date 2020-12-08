db90918e6cf011a0f8dd2f6b2dd208858a8e5724
prashathc <prashathc@gmail.com>
2020-12-08 09:48:47 -0500

    done

--- AssignmentThree_COSC2007/src/BinaryHeapPQ.java
+++ AssignmentThree_COSC2007/src/BinaryHeapPQ.java
@@ -1,6 +1,6 @@
 public class BinaryHeapPQ implements PriorityQueueInterface {
     private Integer[] heap;
-    private static final int MAX_SIZE = 10000;
+    private static final int MAX_SIZE = 11000;
     private static int size = 0;
 
     public BinaryHeapPQ() {
@@ -58,7 +58,7 @@ public class BinaryHeapPQ implements PriorityQueueInterface {
                 bubbleDown(leftChild);                                    // recursively call function with leftChild
             }
 
-        } else if (leftChild == null && rightChild != null) {
+        } else if (rightChild != null && leftChild == null) {
 
             if (heap[i] > heap[rightChild]) {
                 int temp = heap[rightChild];
@@ -67,8 +67,16 @@ public class BinaryHeapPQ implements PriorityQueueInterface {
                 bubbleDown(rightChild);
             }
 
-        } else {                                                    // both children exists
-            int min = heap[leftChild] < heap[rightChild] ? leftChild : rightChild;
+        } else if (leftChild != null && rightChild != null) {        // both children exists
+            int min = 0;
+
+            if (heap[leftChild] <= heap[rightChild]) {
+                min = leftChild;
+            } else {
+                min = rightChild;
+            }
+
+            // int min = heap[leftChild] <= heap[rightChild] ? leftChild : rightChild;
 
             if (heap[i] > heap[min]) {
                 int temp = heap[min];
--- AssignmentThree_COSC2007/src/TestPriorityQueues.java
+++ AssignmentThree_COSC2007/src/TestPriorityQueues.java
@@ -4,15 +4,20 @@ public class TestPriorityQueues {
         BinaryHeapPQ binaryHeapPQ = new BinaryHeapPQ();
 
         for (int i = 0; i < 10000; i++) {
-            int random = (int) (Math.random() * (10000 - 50 + 1)) + 50 ;
+            int random = (int) (Math.random() * (50000 - 0 + 1));
             binaryHeapPQ.insert(random);
             linkedListQueue.insert(random);
         }
 
-        System.out.println(binaryHeapPQ.size());
-        System.out.println(linkedListQueue.size());
+        System.out.println("Size of LinkedList priority queue is: " + linkedListQueue.size());
+        System.out.println("Size of BinaryHeap priority queue is: " + binaryHeapPQ.size());
+
 
         System.out.println("Smallest item in LinkedList: " + linkedListQueue.peek());
         System.out.println("Smallest item in BinaryHeap: " + binaryHeapPQ.peek());
+
+        linkedListQueue.delete();
+        System.out.println("Smallest item in LinkedList after delete: " + linkedListQueue.peek());
+        binaryHeapPQ.delete();
     }
 }
